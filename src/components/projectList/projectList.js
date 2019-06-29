1a4fb6bb9dea6bfe2152cad34b4cd4e1ea8e7e38
Anastasiya Flynn <anastasiyar29@gmail.com>
2019-06-29 13:02:56 -0400

    Hook up 'recent project' links

--- src/components/projectList/projectList.js
+++ src/components/projectList/projectList.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 const ProjectList = () => (
   <>
-    <p>jss-sandbox</p>
-    <p>yeoman</p>
-    <p>shared praceholders</p>
-    <p>Coveo</p>
+    <p>
+      <Link to="/jss-sandbox">Sitecore JavaScript Services Sandbox</Link>
+    </p>
+    <p>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/anastasiya29/yeoman-sitecore-generator"
+      >
+        Sitecore Project Generator (Yeoman)
+      </a>
+    </p>
+    <p>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://marketplace.sitecore.net/en/Modules/S/Shared_Placeholders.aspx"
+      >
+        Sitecore Module: Shared Placeholders
+      </a>
+    </p>
+    <p>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/anastasiya29/coveo-components"
+      >
+        Coveo
+      </a>
+    </p>
   </>
 );
 
