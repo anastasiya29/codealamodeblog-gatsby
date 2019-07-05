735e5c1d079aa73d6f9e6e0e0c1d84fd978d7525
Anastasiya Flynn <anastasiyar29@gmail.com>
2019-07-05 15:24:59 -0400

    prettier fixes

--- src/helpers/pageCreator.js
+++ src/helpers/pageCreator.js
@@ -10,9 +10,9 @@ const createPosts = (posts, createPage, template) =>
       context: {
         slug: getSlug(post),
         left: i === 0 ? null : getSlug(posts[i - 1]),
-        right: i === (posts.length - 1) ? null : getSlug(posts[i + 1]),
+        right: i === posts.length - 1 ? null : getSlug(posts[i + 1]),
       },
-    })
+    });
   });
 
 const createTags = (posts, createPage, template) => {
@@ -21,18 +21,20 @@ const createTags = (posts, createPage, template) => {
     tags = tags.concat(post.node.frontmatter.tags);
   });
 
-  (new Set(tags)).forEach(tag => createPage({
-    path: tagSlugGenerator(tag),
-    component: template,
-    context: {
-      tag
-    }
-  }));
+  new Set(tags).forEach(tag =>
+    createPage({
+      path: tagSlugGenerator(tag),
+      component: template,
+      context: {
+        tag,
+      },
+    })
+  );
 };
 
 const createPostLists = (posts, createPage, template) => {
   const postsPerPage = 6;
-  const numPages = Math.ceil(posts.length / postsPerPage)
+  const numPages = Math.ceil(posts.length / postsPerPage);
   Array.from({ length: numPages }).forEach((_, i) => {
     createPage({
       path: i === 0 ? `/posts` : `/posts/${i + 1}`,
@@ -43,8 +45,8 @@ const createPostLists = (posts, createPage, template) => {
         numPages,
         currentPage: i + 1,
       },
-    })
-  })
+    });
+  });
 };
 
 module.exports = { createPosts, createTags, createPostLists };
